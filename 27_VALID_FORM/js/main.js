312764cf2eada57289c6550978210fec4c907347
Dj-Rom <118632628+Dj-Rom@users.noreply.github.com>
2023-04-24 23:01:46 +0200

    Update main.js

--- 27_VALID_FORM/js/main.js
+++ 27_VALID_FORM/js/main.js
@@ -3,7 +3,7 @@
 const p = document.querySelectorAll("p"),
   form = document.querySelector("form"),
   radio = document.getElementsByName('public');
-  let checkedOn = '';
+  let checkedOn = Boolean;
  
  
 
@@ -29,45 +29,53 @@ function validate(EO) {
   const personsSiteValueDetector = document.querySelector("#persons");
 
   const emailSite = form.elements.email;
-  const emailSiteValue = emailSite.vEO;
+  const emailSiteValue = emailSite.value;
   const emailSiteValueDetector = document.querySelector("#email");
 
   const rubricSite = form.elements.rubric;
-  const rubricSiteValue = rubricSite.value;
+  
+  const rubricChange = document.getElementById('rubric1').selectedIndex;
   const rubricSiteValueDetector = document.querySelector("#rubric");
 
   const publicSite = form.elements.public;
   const publicSiteValue = publicSite.value;
   const publicSiteValueDetector = document.querySelector("#public");
 
-  const commentsSite = form.elements.article;
+  const commentsSite = form.elements.comments;
+  const coomentChecked = document.getElementById("commentS").checked;
   const commentsSiteValue = commentsSite.value;
-  const commentsSiteValueDetector = document.querySelector("#article");
+  const commentsSiteValueDetector = document.querySelector("#comments");
+
+  const articleSite = form.elements.article;
+  const articleSiteValue = articleSite.value;
+  const articleSiteValueDetector = document.querySelector("#article");
 
   valConrole(nameSiteValue, nameSiteValueDetector, nameSite);
   valConrole(urlSiteValue, urlSiteValueDetector, urlSite);
   valConrole(startdateSiteValue, startdateSiteValueDetector, startdateSite);
   valConrole(personsSiteValue, personsSiteValueDetector, personsSite);
   valConrole(emailSiteValue, emailSiteValueDetector, emailSite);
-  valConrole(rubricSiteValue, rubricSiteValueDetector, rubricSite);
+  valConrole(rubricChange , rubricSiteValueDetector, rubricSite);
   valConrole(publicSiteValue, publicSiteValueDetector, publicSite);
-  valConrole(commentsSiteValue, commentsSiteValueDetector, commentsSite);
-
- 
+  valConrole(coomentChecked, commentsSiteValueDetector, commentsSite);
+  valConrole(articleSiteValue, articleSiteValueDetector, articleSite);
+ console.log(coomentChecked);
 
   function valConrole(e, p, f) {
-    if (!e) {
-      p.style.display = "block";
-      f.focus;
+    
+    
+    if(!e){
       EO.preventDefault();
-    } 
-  }
+      p.style.display = "block";
+      f=focus;
+    }
+
   
-  form.removeEventListener("submit", validate, false);
-}
+ 
+}}
 
 form.addEventListener("submit", validate, false);
-form.addEventListener("click" && "focusout", () => {
+form.addEventListener("mousedup"&&"focusout", () => {
   const form = document.querySelector("form");
   const parag = form.querySelectorAll("p");
 
