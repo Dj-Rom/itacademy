e40a4a9715c886fe2d65f98b567cbf31a16fbeda
Dj-Rom <118632628+Dj-Rom@users.noreply.github.com>
2023-04-24 17:30:31 +0200

    Update main.js

--- 27_VALID_FORM/js/main.js
+++ 27_VALID_FORM/js/main.js
@@ -1,96 +1,83 @@
 "use strict";
-const p = document.querySelectorAll('p'),
-    form = document.querySelector('form');
 
-
-
+const p = document.querySelectorAll("p"),
+  form = document.querySelector("form"),
+  radio = document.getElementsByName('public');
+  let checkedOn = '';
+ 
+ 
 
 function validate(EO) {
-    EO = EO || window.event;
+  EO = EO || window.event;
 
+  const form = document.querySelector("form");
 
-    const form = document.querySelector('form')
+  const nameSite = form.elements.name;
+  const nameSiteValue = nameSite.value;
+  const nameSiteValueDetector = document.querySelector("#name");
 
-    const nameSite = form.elements.name;
-    const nameSiteValue = nameSite.value;
-    const nameSiteValueDetector = document.querySelector('#name');
+  const urlSite = form.elements.url;
+  const urlSiteValue = urlSite.value;
+  const urlSiteValueDetector = document.querySelector("#url");
 
+  const startdateSite = form.elements.startdate;
+  const startdateSiteValue = startdateSite.value;
+  const startdateSiteValueDetector = document.querySelector("#startdate");
 
-    const urlSite = form.elements.url;
-    const urlSiteValue = urlSite.value;
-    const urlSiteValueDetector = document.querySelector('#url');
+  const personsSite = form.elements.persons;
+  const personsSiteValue = personsSite.value;
+  const personsSiteValueDetector = document.querySelector("#persons");
 
-    const startdateSite = form.elements.startdate;
-    const startdateSiteValue = startdateSite.value;
-    const startdateSiteValueDetector = document.querySelector('#startdate');
+  const emailSite = form.elements.email;
+  const emailSiteValue = emailSite.vEO;
+  const emailSiteValueDetector = document.querySelector("#email");
 
-    const personsSite = form.elements.persons;
-    const personsSiteValue = personsSite.value;
-    const personsSiteValueDetector = document.querySelector('#persons');
+  const rubricSite = form.elements.rubric;
+  const rubricSiteValue = rubricSite.value;
+  const rubricSiteValueDetector = document.querySelector("#rubric");
 
-    const emailSite = form.elements.email;
-    const emailSiteValue = emailSite.value;
-    const emailSiteValueDetector = document.querySelector('#email');
+  const publicSite = form.elements.public;
+  const publicSiteValue = publicSite.value;
+  const publicSiteValueDetector = document.querySelector("#public");
 
-    const rubricSite = form.elements.rubric;
-    const rubricSiteValue = rubricSite.value;
-    const rubricSiteValueDetector = document.querySelector('#rubric');
+  const commentsSite = form.elements.article;
+  const commentsSiteValue = commentsSite.value;
+  const commentsSiteValueDetector = document.querySelector("#article");
 
-    const publicSite = form.elements.public;
-    const publicSiteValue = publicSite.value;
-    const publicSiteValueDetector = document.querySelector('#public');
+  valConrole(nameSiteValue, nameSiteValueDetector, nameSite);
+  valConrole(urlSiteValue, urlSiteValueDetector, urlSite);
+  valConrole(startdateSiteValue, startdateSiteValueDetector, startdateSite);
+  valConrole(personsSiteValue, personsSiteValueDetector, personsSite);
+  valConrole(emailSiteValue, emailSiteValueDetector, emailSite);
+  valConrole(rubricSiteValue, rubricSiteValueDetector, rubricSite);
+  valConrole(publicSiteValue, publicSiteValueDetector, publicSite);
+  valConrole(commentsSiteValue, commentsSiteValueDetector, commentsSite);
 
-    const commentsSite = form.elements.comments;
-    const commentsSiteValue = commentsSite.value;
-    const commentsSiteValueDetector = document.querySelector('#article');
+ 
 
-
-    valConrole(nameSiteValue, nameSiteValueDetector, nameSite);
-    valConrole(urlSiteValue, urlSiteValueDetector, urlSite);
-    valConrole(startdateSiteValue, startdateSiteValueDetector, startdateSite);
-    valConrole(personsSiteValue, personsSiteValueDetector, personsSite);
-    valConrole(emailSiteValue, emailSiteValueDetector, emailSite)
-    valConrole(rubricSiteValue, rubricSiteValueDetector, rubricSite);
-    valConrole(publicSiteValue, publicSiteValueDetector, publicSite);
-    valConrole(commentsSiteValue, commentsSiteValueDetector, commentsSite);
-
-
-    function check() {
-        document.getElementsByTagName("public").checked
-
-    }
-
-    console.log(check());
-    function valConrole(e, p, f) {
-        if (!e) {
-            p.style.display = 'block';
-            f.focus;
-            EO.preventDefault();
-        } else {
-            p.style.display = 'none';
-
-        }
-
-
-    } form.removeEventListener('submit', validate, false);
+  function valConrole(e, p, f) {
+    if (!e) {
+      p.style.display = "block";
+      f.focus;
+      EO.preventDefault();
+    } 
+  }
+  
+  form.removeEventListener("submit", validate, false);
 }
-form.addEventListener('submit', validate, false);
-form.addEventListener('click' && 'focusout', () => {
-    const form = document.querySelector('form');
-    const parag = form.querySelectorAll('p');
 
+form.addEventListener("submit", validate, false);
+form.addEventListener("click" && "focusout", () => {
+  const form = document.querySelector("form");
+  const parag = form.querySelectorAll("p");
 
-
-    for (let elem of parag) {
-        if (elem.id === event.target.name) {
-            if (!event.target.value) {
-                elem.style.display = 'block';
-
-            } else if (event.target.value) { elem.style.display = 'none'; }
-        }
-
+  for (let elem of parag) {
+    if (elem.id === event.target.name) {
+      if (!event.target.value) {
+        elem.style.display = "block";
+      } else if (event.target.value) {
+        elem.style.display = "none";
+      }
     }
-})
-
-
-
+  }
+});
