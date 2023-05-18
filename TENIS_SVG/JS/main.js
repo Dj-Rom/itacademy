72eb2d9aaa10f890faa779085114879bfe4c039c
Dj-Rom <118632628+Dj-Rom@users.noreply.github.com>
2023-05-18 17:53:51 +0200

    Update main.js

--- TENIS_SVG/JS/main.js
+++ TENIS_SVG/JS/main.js
@@ -6,7 +6,7 @@ contener.id = "contener";
 let btn = document.createElement("button");
 btn.id = "btn";
 let chet = document.createElement('div');
-    chet.id="chet";
+chet.id = "chet";
 let svgBtn = document.createElementNS("http://www.w3.org/2000/svg", "svg");
 svgBtn.id = "svgBtn";
 let svgBtnR = document.createElementNS("http://www.w3.org/2000/svg", "rect");
@@ -21,7 +21,7 @@ let ball = document.createElementNS("http://www.w3.org/2000/svg", "circle");
 ball.id = "ball";
 let racketLeft = document.createElementNS("http://www.w3.org/2000/svg", "rect");
 racketLeft.id = "racketLeft";
-let racketRight = document.createElementNS("http://www.w3.org/2000/svg","rect");
+let racketRight = document.createElementNS("http://www.w3.org/2000/svg", "rect");
 racketRight.id = "racketRight";
 
 
@@ -121,8 +121,8 @@ field = {
     width: 1200,
     height: 600,
     fill: "green",
-    strokeWidth:5,
-    stroke:'black',
+    strokeWidth: 5,
+    stroke: 'black',
     y: 100,
     update: function () {
         const field = document.querySelector("#field");
@@ -144,7 +144,7 @@ ball = {
     speedX: 0,
     speedY: 0,
     CY: 400,
-    Y:400,
+    Y: 400,
     update: function () {
         const ball = document.querySelector("#ball");
         ball.setAttribute("cx", this.CX);
@@ -153,7 +153,7 @@ ball = {
         ball.setAttribute("x", this.X);
         ball.setAttribute("fill", this.fill);
         ball.setAttribute("r", this.r);
-        
+
     },
 };
 ball.update();
@@ -162,7 +162,7 @@ racketLeft = {
     width: 25,
     height: 150,
     x: 0,
-    speedY:0,
+    speedY: 0,
     y: 50,
     fill: "blue",
     update: function () {
@@ -181,7 +181,7 @@ racketRight = {
     height: 150,
     x: 1175,
     y: 200,
-    speedY:0,
+    speedY: 0,
     fill: "blue",
     update: function () {
         const racketRight = document.querySelector("#racketRight");
@@ -189,26 +189,26 @@ racketRight = {
         racketRight.setAttribute("height", this.height);
         racketRight.setAttribute("fill", this.fill);
         racketRight.setAttribute("x", this.x);
-        racketRight.setAttribute("y", this.y + field.y+this.speedY);
+        racketRight.setAttribute("y", this.y + field.y + this.speedY);
     },
 };
 racketRight.update();
-chet  = {
+chet = {
     userRight: 0,
     userLeft: 0,
-    left : 580,
+    left: 580,
     top: 0,
     position: 'absolute',
-    fontSize : 130,
-    innerHTML :`${0}:${0}`,
-    update: function (){
-       let chet = document.querySelector('#chet')
-       chet.innerHTML = this.innerHTML;
-       chet.style.left =  this.left + "px";
-       chet.style.top = this.top + "px";
-       chet.style.position = this.position;
-       chet.style.fontSize = this.fontSize +"px"
-       
+    fontSize: 130,
+    innerHTML: `${0}:${0}`,
+    update: function () {
+        let chet = document.querySelector('#chet')
+        chet.innerHTML = this.innerHTML;
+        chet.style.left = this.left + "px";
+        chet.style.top = this.top + "px";
+        chet.style.position = this.position;
+        chet.style.fontSize = this.fontSize + "px"
+
     },
 };
 chet.update();
@@ -216,129 +216,131 @@ chet.update();
 const btnS = document.querySelector('#btn')
 btnS.addEventListener('click', start)
 
-function start (){
-    const arrRandonX = [4,5,6,-4,-5,-6]
-    const arrRandonY = [1,2,3,-1,-2,-3]
+function start() {
+    const arrRandonX = [2, 3, -4, -2, -3, 4]
+    const arrRandonY = [1, 2, 3, -1, -2, -3]
     ball.speedX = arrRandonX[Math.floor(Math.random() * 6)];
     ball.speedY = arrRandonY[Math.floor(Math.random() * 6)]
     ball.Y = 400;
-    ball.CX = 600;  
+    ball.CX = 600;
     ball.Y += ball.speedY;
     ball.CX += ball.speedX;
 
-btnS.removeEventListener('click', start)
+    btnS.removeEventListener('click', start)
 }
-setInterval(tick,0)
+setInterval(tick, 0)
 
 function tick() {
-   
-// прверяем выход мяча вверх низ
-    const centerBalY = ball.Y + ball.r; 
-        if (centerBalY >= field.height+field.y) {
-            ball.speedY = -ball.speedY;
-            ball.Y = field.height+field.y - ball.r - field.strokeWidth
-        }
-        if (ball.Y <= field.y+ball.r) {
-            ball.speedY = -ball.speedY;
-            ball.Y = field.y+ball.r+field.strokeWidth;
-        }
-////////////
-ball.Y += ball.speedY;
-ball.CX += ball.speedX;
-racketLeft.y += racketLeft.speedY;
-racketRight.y += racketRight.speedY;
-racketLeft.update();
-racketRight.update();
-ball.update();
-  
-// проверяем выход ракеток за поле
-if (racketRight.y<=7) {
-    racketRight.y = 0;
-    } 
-if (racketRight.y >= field.height - racketRight.height) {
-    racketRight.y = field.height - (racketRight.height );
+
+    // прверяем выход мяча вверх низ
+    const centerBalY = ball.Y + ball.r;
+    if (centerBalY >= field.height + field.y) {
+        ball.speedY = -ball.speedY;
+        ball.Y = field.height + field.y - ball.r - field.strokeWidth
     }
-if (racketLeft.y <= 7) {
-    racketLeft.y = 0;
-    } 
-if (racketLeft.y >= field.height - racketLeft.height) {
-    racketLeft.y = field.height - (racketLeft.height);
+    if (ball.Y <= field.y + ball.r) {
+        ball.speedY = -ball.speedY;
+        ball.Y = field.y + ball.r + field.strokeWidth;
+    }
+    ////////////
+    ball.Y += ball.speedY;
+    ball.CX += ball.speedX;
+    racketLeft.y += racketLeft.speedY;
+    racketRight.y += racketRight.speedY;
+    racketLeft.update();
+    racketRight.update();
+    ball.update();
+
+    // проверяем выход ракеток за поле
+    if (racketRight.y <= 7) {
+        racketRight.y = 0;
+    }
+    if (racketRight.y >= field.height - racketRight.height) {
+        racketRight.y = field.height - (racketRight.height);
+    }
+    if (racketLeft.y <= 7) {
+        racketLeft.y = 0;
+    }
+    if (racketLeft.y >= field.height - racketLeft.height) {
+        racketLeft.y = field.height - (racketLeft.height);
     }
     // //////////////////
-ballFunc()
-    
+    ballFunc()
 
-function controlChetAndPausePlay(){
-    if (ball.CX  + ball.r >= field.width ) {
-        chet.userLeft += 1
-           chet.innerHTML = `${chet.userLeft}:${chet.userRight}`
-           chet.update()
-           ball.CX = field.width-ball.r-0.1
-       }
-if (ball.CX <= ball.r) {
-        chet.userRight += 1
-        ball.CX = ball.r
+
+    function controlChetAndPausePlay() {
+        if (ball.CX + ball.r >= field.width) {
+            chet.userLeft += 1
+            chet.innerHTML = `${chet.userLeft}:${chet.userRight}`
+            chet.update()
+            ball.CX = field.width - ball.r - 0.1
+        }
+        if (ball.CX <= ball.r) {
+            chet.userRight += 1
+            ball.CX = ball.r
             chet.innerHTML = `${chet.userLeft}:${chet.userRight}`
             chet.update()
             ball.speedX = 0
             ball.speedY = 0
-            ball.CX = ball.r+0.1
-           
+            ball.CX = ball.r + 0.1
+
+        }
+        if (ball.CX == ball.r + 0.1 || ball.CX >= field.width - ball.r - 0.1) {
+            ball.speedX = 0
+            ball.speedY = 0
+            racketLeft.speedY = 0
+            racketRight.speedY = 0
+            btnS.addEventListener('click', start)
+        }
     }
-    if (ball.CX == ball.r +0.1 || ball.CX >= field.width - ball.r -0.1) {
-        ball.speedX=0
-        ball.speedY=0
-        racketLeft.speedY=0
-        racketRight.speedY=0
-        btnS.addEventListener('click', start)
-    }}
     controlChetAndPausePlay();
 
-   
-function ballFunc(){
-if (racketRight.y+field.y  =< ball.Y+ball.r && ball.Y-ball.r <= racketRight.y +field.y+ racketRight.height) {
-    if (ball.CX + ball.r > field.width - racketRight.width) {
-        ball.speedX = -ball.speedX;
-        ball.X = field.width  - racketRight.width;
+
+    function ballFunc() {
+        if (racketRight.y + field.y <= ball.Y + ball.r && ball.Y - ball.r <= racketRight.y + field.y + racketRight.height) {
+            if (ball.CX + ball.r > field.width - racketRight.width) {
+                ball.speedX = -ball.speedX;
+                ball.X = field.width - racketRight.width;
+            }
+        }
+        // вылетел ли мяч левее стены?
+        if (racketLeft.y <= ball.Y - field.y + ball.r && ball.Y - field.y - ball.r <= racketLeft.y + racketLeft.height) {
+            if (ball.CX - ball.r < racketLeft.width) {
+                ball.speedX = -ball.speedX;
+                ball.X = racketLeft.width - racketRight.width;
+            }
         }
     }
- // вылетел ли мяч левее стены?
-if (racketLeft.y =< ball.Y - field.y+ball.r &&  ball.Y - field.y - ball.r <= racketLeft.y + racketLeft.height) {
-    if (ball.CX - ball.r < racketLeft.width) {
-        ball.speedX = -ball.speedX;
-        ball.X =  racketLeft.width - racketRight.width;
-        }
-    }  
-}
 }
 document.addEventListener("keydown", keyD);
 function keyD() {
     event.preventDefault();
     // блокирум ракетки вслучаи гола
-    if (ball.CX == ball.r +0.1 || ball.CX >= field.width - ball.r -0.1) {
-        racketLeft.speedY=0
-        racketRight.speedY=0}
-
-
-   else if (event.key === "ArrowUp") {
-      racketRight.speedY = -10;
-    }
-   else if (event.key === "ArrowDown") {
-        racketRight.speedY = 10;
+    if (ball.CX == ball.r + 0.1 || ball.CX >= field.width - ball.r - 0.1) {
+        racketLeft.speedY = 0
+        racketRight.speedY = 0
     }
 
-   else if (event.key === "Shift") {
-         racketLeft.speedY = -10;
+
+    else if (event.key === "ArrowUp") {
+        racketRight.speedY = -11;
+    }
+    else if (event.key === "ArrowDown") {
+        racketRight.speedY = 11;
+    }
+
+    else if (event.key === "Shift") {
+        racketLeft.speedY = -11;
     }
 
     else if (event.key === "Control") {
-       racketLeft.speedY = 10;
+        racketLeft.speedY = 11;
     }
-   
+
 }
 document.addEventListener("keyup", keyup);
 function keyup() {
-    
+
     event.preventDefault();
     if (event.key === "ArrowUp") {
         racketRight.speedY = 0;
